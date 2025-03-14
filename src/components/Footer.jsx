a623e420c8e56fc668fe5a27c440fc1b8d4f291a
Sujeet17m <sujeet17das@gmail.com>
2025-03-15 00:42:27 +0530

    Update Footer.jsx 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,23 @@ const Footer = () => {
       <div className="container mx-auto text-center">
         <div className="mb-6">
           <h3 className="text-xl font-medium mb-2">For inquiries, contact:</h3>
-          <p className="mb-1">Student Coordinator: +91 9999999999</p>
-          <p>Faculty Coordinator: +91 9999999999</p>
+          {/*           <p className="mb-1">Student Coordinator: +91 9999999999</p>
+          <p>Faculty Coordinator: +91 9999999999</p> */}
+          <p>Staff Coordinators:</p>
+<ul>
+  <li>Dr.G.REVATHY - +91 9566290952</li>
+  <li>Dr.P.THILAKAVATHY - +91 9400446608</li>
+  <li>Ms.S.M.SOWMIYA</li>
+  <li>Ms.S.SATHEA SREE</li>
+</ul>
+<p>Student Coordinators:</p>
+<ul>
+  <li>Mr.S.SANJAY - +91 9087358055</li>
+  <li>Mr.UM.HARIKESARI - +91 8754681336</li>
+  <li>Ms.G.ABINAYA - +91 8838182311</li>
+  <li>Ms.S.PRABAVATHI - +91 9790991806</li>
+</ul>
+
         </div>
         
         <div className="text-sm text-white/60">
